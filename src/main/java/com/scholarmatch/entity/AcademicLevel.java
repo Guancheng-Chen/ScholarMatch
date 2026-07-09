e1234a9d2e020d60e1c68113ab915bf3444f37f2
Guancheng-Chen <guancheng.chen@mail.utoronto.ca>
2026-07-09 13:43:19 -0400

    feat: add AcademicLevel enum skeleton

---
+++ src/main/java/com/scholarmatch/entity/AcademicLevel.java
@@ -0,0 +1,14 @@
+package com.scholarmatch.entity;
+
+/**
+ * Represents the academic career stage of a registered User.
+ */
+public enum AcademicLevel {
+
+    UNDERGRADUATE,
+    GRADUATE_STUDENT,
+    POSTDOCTORAL_RESEARCHER,
+    FACULTY,
+    INDUSTRY_RESEARCHER,
+
+}
