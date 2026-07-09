6c135e85aa522cfd09e534ad442ef1cf4b987fa1
Marian7101 <marian.lni@mail.utoronto.ca>
2026-07-09 16:37:14 -0400

    feat(connect): add CollaborationType enum skeleton

---
+++ src/main/java/com/scholarmatch/entity/CollaborationType.java
@@ -0,0 +1,12 @@
+package com.scholarmatch.entity;
+
+/**
+ * Describes what kind of academic connection a User is looking for on ScholarMatch.
+ */
+public enum CollaborationType {
+    CO_AUTHOR,
+    RESEARCH_GROUP,
+    PEER_REVIEW,
+    MENTORSHIP,
+    INTEREST_SHARING,
+}
