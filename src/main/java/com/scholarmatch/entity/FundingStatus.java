52bc2b5235c0bb560512e2ccc470ca31b33a95c1
Marian7101 <marian.lin@mail.utoronto.ca>
2026-07-10 18:39:02 -0400

    feat(profile): add FundingStatus enum skeleton

---
+++ src/main/java/com/scholarmatch/entity/FundingStatus.java
@@ -0,0 +1,14 @@
+package com.scholarmatch.entity;
+
+/**
+ * Describes how a {@link User}'s research work is currently financially supported.
+ */
+public enum FundingStatus {
+    SELF_FUNDED,
+    INSTITUTIONAL_FUNDING,
+    GOVERNMENT_GRANT,
+    INDUSTRY_SPONSORED,
+    SCHOLARSHIP_FELLOWSHIP,
+    UNFUNDED,
+    OTHER,
+}
