be3145ab981717f8870ca731eb997704a42feef9
Guancheng-Chen <guancheng.chen@mail.utoronto.ca>
2026-07-11 17:46:57 -0400

    feat: add import dependency for RecommendDataAccessInterface

---
+++ src/main/java/com/scholarmatch/usecase/data_access_interface/RecommendDataAccessInterface.java
@@ -0,0 +1,7 @@
+package com.scholarmatch.usecase.data_access_interface;
+
+import com.scholarmatch.entity.User;
+import com.scholarmatch.usecase.exception.ExternalServiceException;
+
+public interface RecommendDataAccessInterface {
+}
