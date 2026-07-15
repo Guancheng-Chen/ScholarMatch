d4250977c7aad1e3e63a1c358d179c6f84b5743a
Marian7101 <marian.lin@mail.utoronto.ca>
2026-07-14 23:26:47 -0400

    feat(skip): add SkipInteractor constructor

--- src/main/java/com/scholarmatch/usecase/skip/SkipInteractor.java
+++ src/main/java/com/scholarmatch/usecase/skip/SkipInteractor.java
@@ -3,4 +3,8 @@ package com.scholarmatch.usecase.skip;
 public final class SkipInteractor implements SkipInputBoundary {
 
     private final SkipOutputBoundary outputBoundary;
+
+    public SkipInteractor(final SkipOutputBoundary outputBoundary) {
+        this.outputBoundary = outputBoundary;
+    }
 }
