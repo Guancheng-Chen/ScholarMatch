53fd19e5ef8b1d2cea4eb3fed87519c66aeb9e81
Marian7101 <marian.lin@mail.utoronto.ca>
2026-07-16 15:30:48 -0400

    feat(connect): add ConnectInteractor constructor

--- src/main/java/com/scholarmatch/usecase/connect/ConnectInteractor.java
+++ src/main/java/com/scholarmatch/usecase/connect/ConnectInteractor.java
@@ -6,4 +6,11 @@ public final class ConnectInteractor implements ConnectInputBoundary {
 
     private final ConnectDataAccessInterface dataAccessObject;
     private final ConnectOutputBoundary outputBoundary;
+
+    public ConnectInteractor(
+            final ConnectDataAccessInterface dataAccessObject,
+            final ConnectOutputBoundary outputBoundary) {
+        this.dataAccessObject = dataAccessObject;
+        this.outputBoundary = outputBoundary;
+    }
 }
