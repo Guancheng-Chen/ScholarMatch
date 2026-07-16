ca2f2d1b77e8002bd549802e2c0884d7bbff74e3
Marian7101 <marian.lin@mail.utoronto.ca>
2026-07-16 15:30:39 -0400

    feat(connect): add interactor dependencies

--- src/main/java/com/scholarmatch/usecase/connect/ConnectInteractor.java
+++ src/main/java/com/scholarmatch/usecase/connect/ConnectInteractor.java
@@ -1,4 +1,9 @@
 package com.scholarmatch.usecase.connect;
 
+import com.scholarmatch.usecase.data_access_interface.ConnectDataAccessInterface;
+
 public final class ConnectInteractor implements ConnectInputBoundary {
+
+    private final ConnectDataAccessInterface dataAccessObject;
+    private final ConnectOutputBoundary outputBoundary;
 }
