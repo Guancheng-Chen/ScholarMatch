658dceb3943bf4a59033a3fd7f152b8c8aa7cebf
alan746 <18900265965@163.com>
2026-07-19 18:57:04 -0400

    feat(update-profile): Inject update profile input boundary

--- src/main/java/com/scholarmatch/interface_adapter/controller/UpdateProfileController.java
+++ src/main/java/com/scholarmatch/interface_adapter/controller/UpdateProfileController.java
@@ -1,7 +1,20 @@
 package com.scholarmatch.interface_adapter.controller;
 
+import com.scholarmatch.usecase.update_profile.UpdateProfileInputBoundary;
+
 /**
  * Controller for the update-profile use case.
  */
 public final class UpdateProfileController {
+
+    private final UpdateProfileInputBoundary interactor;
+
+    /**
+     * Constructs an UpdateProfileController.
+     *
+     * @param interactor the update-profile use case
+     */
+    public UpdateProfileController(final UpdateProfileInputBoundary interactor) {
+        this.interactor = interactor;
+    }
 }
