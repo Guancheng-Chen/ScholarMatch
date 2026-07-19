6018f6afefd2a836676bfc7684612db84078417a
alan746 <18900265965@163.com>
2026-07-19 19:38:08 -0400

    feat(skip): Inject skip input boundary

--- src/main/java/com/scholarmatch/interface_adapter/controller/SkipController.java
+++ src/main/java/com/scholarmatch/interface_adapter/controller/SkipController.java
@@ -1,7 +1,20 @@
 package com.scholarmatch.interface_adapter.controller;
 
+import com.scholarmatch.usecase.skip.SkipInputBoundary;
+
 /**
  * Controller for the skip use case.
  */
 public final class SkipController {
+
+    private final SkipInputBoundary interactor;
+
+    /**
+     * Constructs a SkipController.
+     *
+     * @param interactor the skip use case
+     */
+    public SkipController(final SkipInputBoundary interactor) {
+        this.interactor = interactor;
+    }
 }
