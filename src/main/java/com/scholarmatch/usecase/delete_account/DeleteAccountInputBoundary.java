56c414a1bbd9db0499524c8f93c1d7f9215d8240
alan746 <18900265965@163.com>
2026-07-19 22:30:17 -0400

    docs(delete-account): Document input boundary

--- src/main/java/com/scholarmatch/usecase/delete_account/DeleteAccountInputBoundary.java
+++ src/main/java/com/scholarmatch/usecase/delete_account/DeleteAccountInputBoundary.java
@@ -1,5 +1,8 @@
 package com.scholarmatch.usecase.delete_account;
 
+/**
+ * Input boundary for the delete-account use case.
+ */
 public interface DeleteAccountInputBoundary {
 
     void execute();
