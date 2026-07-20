7c1d5018c981c9b4a96dbce734d6cd55f718524c
alan746 <18900265965@163.com>
2026-07-19 21:54:02 -0400

    feat(delete-account): Inject delete account input boundary

--- src/main/java/com/scholarmatch/interface_adapter/controller/DeleteAccountController.java
+++ src/main/java/com/scholarmatch/interface_adapter/controller/DeleteAccountController.java
@@ -1,7 +1,21 @@
 package com.scholarmatch.interface_adapter.controller;
 
+import com.scholarmatch.usecase.delete_account.DeleteAccountInputBoundary;
+
 /**
  * Controller for the delete-account use case.
  */
 public final class DeleteAccountController {
+
+    private final DeleteAccountInputBoundary interactor;
+
+    /**
+     * Constructs a DeleteAccountController.
+     *
+     * @param interactor the delete-account use case
+     */
+    public DeleteAccountController(
+            final DeleteAccountInputBoundary interactor) {
+        this.interactor = interactor;
+    }
 }
