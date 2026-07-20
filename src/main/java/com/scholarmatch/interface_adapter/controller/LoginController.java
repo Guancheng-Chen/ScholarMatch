3ad60dab0335b40ce6324a55885f303ebbdcc1f8
alan746 <18900265965@163.com>
2026-07-19 21:30:26 -0400

    feat(login): Forward credentials to login use case

--- src/main/java/com/scholarmatch/interface_adapter/controller/LoginController.java
+++ src/main/java/com/scholarmatch/interface_adapter/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.scholarmatch.interface_adapter.controller;
 
 import com.scholarmatch.usecase.login.LoginInputBoundary;
+import com.scholarmatch.usecase.login.LoginInputData;
 
 /**
  * Controller for the login use case.
@@ -16,5 +17,16 @@ public final class LoginController {
      */
     public LoginController(final LoginInputBoundary interactor) {
         this.interactor = interactor;
+    }
+
+    /**
+     * Sends the entered credentials to the login use case.
+     *
+     * @param email the entered email
+     * @param password the entered password
+     */
+    public void login(final String email, final String password) {
+        final LoginInputData inputData = new LoginInputData(email, password);
+        this.interactor.execute(inputData);
     }
 }
