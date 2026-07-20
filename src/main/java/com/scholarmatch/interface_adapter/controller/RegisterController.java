e958f203b3e2a0e457bf60f07f8bfa7e323dba61
alan746 <18900265965@163.com>
2026-07-19 21:07:57 -0400

    feat(register): Forward registration data to use case

--- src/main/java/com/scholarmatch/interface_adapter/controller/RegisterController.java
+++ src/main/java/com/scholarmatch/interface_adapter/controller/RegisterController.java
@@ -1,6 +1,7 @@
 package com.scholarmatch.interface_adapter.controller;
 
 import com.scholarmatch.usecase.register.RegisterInputBoundary;
+import com.scholarmatch.usecase.register.RegisterInputData;
 
 /**
  * Controller for the register use case.
@@ -16,5 +17,22 @@ public final class RegisterController {
      */
     public RegisterController(final RegisterInputBoundary registerInteractor) {
         this.registerInteractor = registerInteractor;
+    }
+
+    /**
+     * Submits registration data to the register use case.
+     *
+     * @param firstName the user's first name
+     * @param lastName  the user's last name
+     * @param email     the user's email
+     * @param password  the user's password
+     */
+    public void execute(
+            final String firstName,
+            final String lastName,
+            final String email,
+            final String password) {
+        this.registerInteractor.execute(
+                new RegisterInputData(firstName, lastName, email, password));
     }
 }
