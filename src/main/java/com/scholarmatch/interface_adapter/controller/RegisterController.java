dc47115e8799b4407b15688fbe135bed0bee0390
alan746 <18900265965@163.com>
2026-07-19 21:07:23 -0400

    feat(register): Inject register input boundary

--- src/main/java/com/scholarmatch/interface_adapter/controller/RegisterController.java
+++ src/main/java/com/scholarmatch/interface_adapter/controller/RegisterController.java
@@ -1,7 +1,20 @@
 package com.scholarmatch.interface_adapter.controller;
 
+import com.scholarmatch.usecase.register.RegisterInputBoundary;
+
 /**
  * Controller for the register use case.
  */
 public final class RegisterController {
+
+    private final RegisterInputBoundary registerInteractor;
+
+    /**
+     * Constructs a RegisterController.
+     *
+     * @param registerInteractor the register use case
+     */
+    public RegisterController(final RegisterInputBoundary registerInteractor) {
+        this.registerInteractor = registerInteractor;
+    }
 }
