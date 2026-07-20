93595fff97fc3c2fa94e63b6e896406129601359
alan746 <18900265965@163.com>
2026-07-19 21:14:36 -0400

    feat(paper-lookup): Inject paper lookup input boundary

--- src/main/java/com/scholarmatch/interface_adapter/controller/PaperLookupController.java
+++ src/main/java/com/scholarmatch/interface_adapter/controller/PaperLookupController.java
@@ -1,7 +1,20 @@
 package com.scholarmatch.interface_adapter.controller;
 
+import com.scholarmatch.usecase.paper_lookup.PaperLookupInputBoundary;
+
 /**
  * Controller for the paper lookup use case.
  */
 public final class PaperLookupController {
+
+    private final PaperLookupInputBoundary paperLookupInteractor;
+
+    /**
+     * Constructs a PaperLookupController.
+     *
+     * @param paperLookupInteractor the paper lookup use case
+     */
+    public PaperLookupController(final PaperLookupInputBoundary paperLookupInteractor) {
+        this.paperLookupInteractor = paperLookupInteractor;
+    }
 }
