f758a7300c09660c7e57cf7dd82e436e82511e17
alan746 <18900265965@163.com>
2026-07-19 21:00:27 -0400

    feat(send-message): Inject send message input boundary

--- src/main/java/com/scholarmatch/interface_adapter/controller/SendMessageController.java
+++ src/main/java/com/scholarmatch/interface_adapter/controller/SendMessageController.java
@@ -1,7 +1,20 @@
 package com.scholarmatch.interface_adapter.controller;
 
+import com.scholarmatch.usecase.send_message.SendMessageInputBoundary;
+
 /**
  * Controller for the send-message use case.
  */
 public final class SendMessageController {
+
+    private final SendMessageInputBoundary interactor;
+
+    /**
+     * Constructs a SendMessageController.
+     *
+     * @param interactor the send-message use case
+     */
+    public SendMessageController(final SendMessageInputBoundary interactor) {
+        this.interactor = interactor;
+    }
 }
