43c214cf2e63d69e289bab8085ef123377760f22
alan746 <18900265965@163.com>
2026-07-19 21:48:02 -0400

    feat(load-matches): Inject load matches input boundary

--- src/main/java/com/scholarmatch/interface_adapter/controller/LoadMatchesController.java
+++ src/main/java/com/scholarmatch/interface_adapter/controller/LoadMatchesController.java
@@ -1,7 +1,21 @@
 package com.scholarmatch.interface_adapter.controller;
 
+import com.scholarmatch.usecase.load_matches.LoadMatchesInputBoundary;
+
 /**
  * Controller for the load-matches use case.
  */
 public final class LoadMatchesController {
+
+    private final LoadMatchesInputBoundary loadMatchesInteractor;
+
+    /**
+     * Constructs a LoadMatchesController.
+     *
+     * @param loadMatchesInteractor the load-matches use case
+     */
+    public LoadMatchesController(
+            final LoadMatchesInputBoundary loadMatchesInteractor) {
+        this.loadMatchesInteractor = loadMatchesInteractor;
+    }
 }
