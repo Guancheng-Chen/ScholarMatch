2c2d87919e20c5d4a935f4e6ffd34a5238b41c1f
alan746 <18900265965@163.com>
2026-07-19 21:10:55 -0400

    feat(recommend): Add recommend input boundary dependency

--- src/main/java/com/scholarmatch/interface_adapter/controller/RecommendController.java
+++ src/main/java/com/scholarmatch/interface_adapter/controller/RecommendController.java
@@ -1,7 +1,11 @@
 package com.scholarmatch.interface_adapter.controller;
 
+import com.scholarmatch.usecase.recommend.RecommendInputBoundary;
+
 /**
  * Controller for the recommendation use case.
  */
 public final class RecommendController {
+
+    private final RecommendInputBoundary recommendInteractor;
 }
