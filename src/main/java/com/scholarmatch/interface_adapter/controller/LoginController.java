70f418ce2e38b1908d36b41ca4ed28172d0c4afb
alan746 <18900265965@163.com>
2026-07-19 21:30:11 -0400

    feat(login): Inject login input boundary

--- src/main/java/com/scholarmatch/interface_adapter/controller/LoginController.java
+++ src/main/java/com/scholarmatch/interface_adapter/controller/LoginController.java
@@ -1,7 +1,20 @@
 package com.scholarmatch.interface_adapter.controller;
 
+import com.scholarmatch.usecase.login.LoginInputBoundary;
+
 /**
  * Controller for the login use case.
  */
 public final class LoginController {
+
+    private final LoginInputBoundary interactor;
+
+    /**
+     * Constructs a LoginController.
+     *
+     * @param interactor the login use case
+     */
+    public LoginController(final LoginInputBoundary interactor) {
+        this.interactor = interactor;
+    }
 }
