cd553a52421941a5269b855a66f4b35826fafd48
alan746 <18900265965@163.com>
2026-07-19 21:44:51 -0400

    feat(load-message): Inject load message input boundary

--- src/main/java/com/scholarmatch/interface_adapter/controller/LoadMessageController.java
+++ src/main/java/com/scholarmatch/interface_adapter/controller/LoadMessageController.java
@@ -1,7 +1,20 @@
 package com.scholarmatch.interface_adapter.controller;
 
+import com.scholarmatch.usecase.load_message.LoadMessageInputBoundary;
+
 /**
  * Controller for the load-message use case.
  */
 public final class LoadMessageController {
+
+    private final LoadMessageInputBoundary interactor;
+
+    /**
+     * Constructs a LoadMessageController.
+     *
+     * @param interactor the load-message use case
+     */
+    public LoadMessageController(final LoadMessageInputBoundary interactor) {
+        this.interactor = interactor;
+    }
 }
