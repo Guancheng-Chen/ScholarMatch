4cfe288324f32d89b797e1d63d33bb1e106facba
alan746 <18900265965@163.com>
2026-07-19 21:38:49 -0400

    feat(logout): Inject logout input boundary

--- src/main/java/com/scholarmatch/interface_adapter/controller/LogoutController.java
+++ src/main/java/com/scholarmatch/interface_adapter/controller/LogoutController.java
@@ -1,7 +1,20 @@
 package com.scholarmatch.interface_adapter.controller;
 
+import com.scholarmatch.usecase.logout.LogoutInputBoundary;
+
 /**
  * Controller for the logout use case.
  */
 public final class LogoutController {
+
+    private final LogoutInputBoundary interactor;
+
+    /**
+     * Constructs a LogoutController.
+     *
+     * @param interactor the logout use case
+     */
+    public LogoutController(final LogoutInputBoundary interactor) {
+        this.interactor = interactor;
+    }
 }
