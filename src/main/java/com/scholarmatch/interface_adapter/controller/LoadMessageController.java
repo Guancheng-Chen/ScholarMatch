ce4f562538ae5f97a68bf3d8db7e1faaf123bd23
alan746 <18900265965@163.com>
2026-07-19 21:45:12 -0400

    feat(load-message): Forward conversation load request

--- src/main/java/com/scholarmatch/interface_adapter/controller/LoadMessageController.java
+++ src/main/java/com/scholarmatch/interface_adapter/controller/LoadMessageController.java
@@ -1,6 +1,7 @@
 package com.scholarmatch.interface_adapter.controller;
 
 import com.scholarmatch.usecase.load_message.LoadMessageInputBoundary;
+import com.scholarmatch.usecase.load_message.LoadMessageInputData;
 
 /**
  * Controller for the load-message use case.
@@ -16,5 +17,17 @@ public final class LoadMessageController {
      */
     public LoadMessageController(final LoadMessageInputBoundary interactor) {
         this.interactor = interactor;
+    }
+
+    /**
+     * Loads messages exchanged with another user.
+     *
+     * @param otherUserId the other user's ID
+     */
+    public void loadMessages(final String otherUserId) {
+        final LoadMessageInputData inputData =
+                new LoadMessageInputData(otherUserId);
+
+        this.interactor.execute(inputData);
     }
 }
