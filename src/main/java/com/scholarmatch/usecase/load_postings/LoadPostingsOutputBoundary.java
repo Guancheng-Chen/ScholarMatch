cbdd08f712aad3d22fe5c86d8d487296ffac7e10
Marian7101 <marian.lin@mail.utoronto.ca>
2026-07-21 23:43:44 -0400

    docs(load-postings): document output boundary

--- src/main/java/com/scholarmatch/usecase/load_postings/LoadPostingsOutputBoundary.java
+++ src/main/java/com/scholarmatch/usecase/load_postings/LoadPostingsOutputBoundary.java
@@ -1,7 +1,21 @@
 package com.scholarmatch.usecase.load_postings;
 
+/**
+ * Output boundary for the load postings use case.
+ */
 public interface LoadPostingsOutputBoundary {
+
+    /**
+     * Prepares the success view.
+     *
+     * @param outputData the output data for the view
+     */
     void prepareSuccessView(LoadPostingsOutputData outputData);
 
+    /**
+     * Prepares the failure view.
+     *
+     * @param error the error message to display
+     */
     void prepareFailView(String error);
 }
