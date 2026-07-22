ac8528878218b744b50c0cdc0dc94d954c5689db
Marian7101 <marian.lin@mail.utoronto.ca>
2026-07-21 23:40:31 -0400

    docs(load-postings): document input data

--- src/main/java/com/scholarmatch/usecase/load_postings/LoadPostingsInputData.java
+++ src/main/java/com/scholarmatch/usecase/load_postings/LoadPostingsInputData.java
@@ -1,4 +1,9 @@
 package com.scholarmatch.usecase.load_postings;
 
+/**
+ * Input data for loading postings.
+ *
+ * @param scope the requested posting scope
+ */
 public record LoadPostingsInputData(PostingScope scope) {
 }
