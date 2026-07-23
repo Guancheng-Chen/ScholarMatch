6fe7ed0663c3d163bcd066557209be0d18b028e7
Guancheng-Chen <guancheng.chen@mail.utoronto.ca>
2026-07-22 22:04:22 -0400

    refactor:delect unnecessary files

--- src/main/java/com/scholarmatch/usecase/data_access_interface/VerificationEmailSenderDataAccessInterface.java
+++
@@ -1,9 +0,0 @@
-package com.scholarmatch.usecase.data_access_interface;
-
-/**
- * Boundary for delivering a registration verification code.
- */
-public interface VerificationEmailSenderDataAccessInterface {
-
-    void sendVerificationCode(String email, String code);
-}
